d60c2805fa09b037b41e9d8cdf127a75b1d4e0fb
Scott Lawrence <bytbox@gmail.com>
2010-12-19 18:05:33 -0500

    reading from the hard drive works

--- bootloader/wl.asm
+++ bootloader/wl.asm
@@ -7,12 +7,6 @@ cli
 
 call	Progress
 
-;mov     ax, 0x07c0
-;mov     ds, ax
-;mov     es, ax
-;mov     fs, ax
-;mov     gs, ax
-
 ; Assume we're using the first hard disk
 mov	dl, 0x80
 
@@ -20,28 +14,20 @@ mov	dl, 0x80
 mov	ah, 0x00
 int	0x13
 
-;mov	di, 512
-;a:
-;mov	al, [0x7c00 + di]
-;call	PrintCharacter
-;dec	di
-;jnz	a
-
 mov	si, 0x7c00
 add	si, 0x0200
 sub	si, 18
 
-call	PrintCharacter
 mov	ah, 0x42
 int	0x13
+jc	Error
 
-mov	al, ah
+mov	al, [0x1000]
 add	al, '0'
 call	PrintCharacter
 
-jc	Error
-
 call	Progress
+
 hlt
 
 ;; print a '.' to the screen
@@ -70,13 +56,15 @@ times	512 - 16 - 2 - ($ - $$) db 0
 ; The DAP: Data Address Packet
 db	0x10	; size of DAP
 db	0x00	; unused
-db	0x01	; number of sectors to read
-db	0x00	; unused
+dw	0x01	; number of sectors to read
 dw	0x1000	; offset of destination buffer
 dw	0x0000	; segment of destination buffer
-dw	0x0000
-dw	0x0000
-dw	0x0000
-dw	0x0001	; where to start reading
+dq	0x0001	; where to start reading
+
 dw	0xaa55
+
+call	Progress
+
+; force completion of the sector
+times	512 db 0
 
