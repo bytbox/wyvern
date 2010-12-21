f0ca30db60239f9b85deb27a850cc4ac8b42d4e1
Scott Lawrence <bytbox@gmail.com>
2010-12-20 23:35:46 -0500

    making stage2 print out some information

--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -5,8 +5,11 @@
 [bits	16]
 [org	0x1000]
 
-mov	al, '>'
-call	PrintCharacter
+mov	si, $name
+call	PrintString
+call	Newline
+
+; get information about our system
 
 ; TODO: go graphical
 
@@ -23,13 +26,38 @@ or	al, 1
 ;;	These functions are 16-bit real mode functions - they
 ;;	use the BIOS directly and will not work once we are in protected mode.
 
-; Print a character to the screen
+; Print a character to the screen from al
 PrintCharacter:
 	mov	ah, 0x0e
 	mov	bh, 0x00
 	mov	bl, 0x07
 	int	0x10
 	ret
+
+; Print a (null-terminated) string to the screen from the address in si
+PrintString:
+	mov	ch, 0
+	mov	cl, 0
+	.1:
+	mov	bx, cx
+	mov	al, [si + bx]
+	call	PrintCharacter
+	inc	cl
+	mov	bx, cx
+	mov	al, [si + bx]
+	cmp	al, 0
+	jnz	.1
+	ret
+
+; Move to the beginning of the next line
+Newline:
+	mov	al, 10
+	call	PrintCharacter
+	mov	al, 13
+	call	PrintCharacter
+	ret
+
+$name:		db	'Wyvern Bootloader v0.1'
 
 ; force a total of 7 sectors
 times	7*512 - ($ - $$) db 0
