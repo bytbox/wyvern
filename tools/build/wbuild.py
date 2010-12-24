e2d63ec643f480176a08fb1db0c0d74db535fb53
Scott Lawrence <bytbox@gmail.com>
2010-12-23 21:55:22 -0500

    verbose option added

--- tools/build/wbuild.py
+++ tools/build/wbuild.py
@@ -2,3 +2,13 @@
 
 from optparse import OptionParser
 
+# Find configuration file
+
+# Parse options
+usage = "usage: %prog [options]"
+parser = OptionParser(usage=usage)
+parser.add_option("-v", "--verbose", action="store_true",
+	dest="verbose", default=False, 
+	help="enable verbose output");
+(options, args) = parser.parse_args()
+
