72039aa5530dce1ccf2158e2c3bb9fa0d58e9e62
Scott Lawrence <bytbox@gmail.com>
2011-03-20 19:57:34 -0400

    working on debugging the memory positioning problem

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,7 +1,19 @@
+char d = 'r';
 char h = 'H';
 void kmain() {
 	unsigned char *videoram = (unsigned char *)0xb8000;
 	videoram[0]=h;
-	videoram[2]='i';
+	int loc = (int)&h;
+	videoram[2]=loc;
+	char out[12];
+	int i;
+	for (i=0; i<12; i++) {
+		int x = loc%8;
+		out[i] = '0'+x;
+		loc /= 8;
+	}
+
+	for (i=0; i<12; i++)
+		videoram[i*2] = out[i];
 }
 
