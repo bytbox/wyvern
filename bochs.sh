01c962899a524068a5e98101bd21124b54b8c832
Scott Lawrence <bytbox@gmail.com>
2011-10-17 04:27:08 -0400

    Getting qemu and bochs to work again

--- bochs.sh
+++ bochs.sh
@@ -4,7 +4,7 @@
 dd if=/dev/zero of=pad bs=1 count=750 > /dev/null 2> /dev/null
 cp tools/grub/boot/grub/stage1 .
 cp tools/grub/boot/grub/stage2 .
-cat stage1 stage2 pad wyvern-0.0.1.img > floppy.img 
+cat stage1 stage2 pad wyvern.img > floppy.img 
 
 rm pad stage1 stage2
 
